1a3a01ba905e55cd90d0f9487596ce90ab3e25ca
renjiwangzi <renjiwangzi@gmail.com>
2023-11-07 13:15:23 +0800

    Update app_edtunnel.js

--- app_edtunnel.js
+++ app_edtunnel.js
@@ -4,7 +4,7 @@ const net = require('net');
 
 // 创建 WebSocket 服务器
 const wss = new WebSocket.Server({ port: 443 });
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = '5bfef9e0-91d5-457f-a7f8-6c9af35131d5;
 let proxyIP = "64.68.192." + Math.floor(Math.random() * 255);
 
 let address = '';
